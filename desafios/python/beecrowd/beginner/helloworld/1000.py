7a3a6d7a8465d7208c56a4395025648396219890
Jéssica Abrantes <152931139+JessicaCAbrantes@users.noreply.github.com>
2025-10-01 11:06:19 -0300

    Update 1000.py

---
+++ desafios/python/beecrowd/beginner/helloworld/1000.py
@@ -0,0 +1,17 @@
+'''
+Problema #1000 - Hello World! - Nível 5
+
+beecrowd | 1000
+Hello World!
+Adapted by Jean Bez, beecrowd !- Brasil
+
+You must print the message Hello World! and then the endline as shown below.
+
+Solução em Pyhton 3.11:
+
+| Input Sample | Output Sample |
+| ------------ | ------------- |
+|              | Hello World!  |
+'''
+
+print("Hello World!")
